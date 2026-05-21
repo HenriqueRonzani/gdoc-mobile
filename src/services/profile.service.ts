4955fbdcc9ac1c450b3f8c8031b36864bda17794
Henrique <henriquedasilvaronzani@gmail.com>
2026-05-21 09:41:28 -0300

    GM-9: Removendo console.log

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,11 +1,6 @@
 import api from '@/lib/axios'
 
 export const getProfile = async () => {
-
-  console.log('TOKEN:')
-  console.log(api.defaults.headers.common.Authorization)
-
   const response = await api.get('/auth/me')
-
   return response.data
 }
