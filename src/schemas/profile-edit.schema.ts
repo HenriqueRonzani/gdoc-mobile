352d5ce4f32f0f74fe58de68d51c4ae84e2c66b6
Henrique <henriquedasilvaronzani@gmail.com>
2026-06-17 20:20:59 -0300

    GM-22: Corrigindo erro de data de nascimento inválida na atualização de cadastro

--- src/schemas/profile-edit.schema.ts
+++ src/schemas/profile-edit.schema.ts
@@ -1,6 +1,5 @@
 import { z } from 'zod'
 import { obligatoryStringField, fullNameField } from './common.schema'
-import dayjs from 'dayjs'
 
 export const AddressEditSchema = z.object({
   street: obligatoryStringField.optional(),
@@ -25,7 +24,7 @@ export const UpdateUserProfileSchema = z.object({
   place_of_birth: z.string().nullable().optional(),
   mother_name: z.string().nullable().optional(),
   cpfCnpj: z.string().optional(),
-  dateOfBirth: z.string().optional().transform(value => dayjs(value, 'DD/MM/YYYY').format('YYYY-MM-DD')),
+  dateOfBirth: z.string().optional(),
 
   contacts: z.array(ContactEditSchema).optional(),
 
